69487680346f5ca870b582b3d8e86286466b84cb
indiaklint <70303365+indiaklint@users.noreply.github.com>
2020-08-27 06:36:32 -0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -6,8 +6,8 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
 
-API_KEY = input("API_KEY: ")
-API_HASH = input("API_HASH: ")
+API_KEY = input("1037055457: ")
+API_HASH = input("AAEV8AYOpogwh-dUiu2aJ0NGqYyboWXkzlI: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Here is your userbot srting, copy it to a safe place !!")
